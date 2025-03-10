a9720fdd101a6233e8415e62d6b8845ed3731008
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-10 13:18:43 +0100

    FIX🔧 Corrected relationship in Domain  entity class

--- API/Entities/Domain.cs
+++ API/Entities/Domain.cs
@@ -6,8 +6,8 @@ namespace API.Entities{
         public int Id {get;set;}
         [Column("DOMA_name")]
         public required string Name {get;set;}
-
-        // Définit la relation avec les domaines (Un post peut avoir plusieurs domaines).
-        public ICollection<PostDomain>Domains {get;set;} = new List<PostDomain>(); //Liste des domaines associés
+        
+        // Relation Many-to-Many entre Domain et Post via PostDomain
+        public ICollection<PostDomain>PostDomains {get;set;} = new List<PostDomain>();
     }
 }
