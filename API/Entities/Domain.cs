20e62d33464632691d4fda1b95dd8a9ed54d6b61
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-10 13:06:47 +0100

    STRUCT 📁 Added Domain  Relation with Post Class

--- API/Entities/Domain.cs
+++ API/Entities/Domain.cs
@@ -1,10 +1,13 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace firstAspnet.Domain.Entities{
+namespace API.Entities{
     public class Domain{
         [Column("DOMA_id")]
         public int Id {get;set;}
         [Column("DOMA_name")]
         public required string Name {get;set;}
+
+        // Définit la relation avec les domaines (Un post peut avoir plusieurs domaines).
+        public ICollection<PostDomain>Domains {get;set;} = new List<PostDomain>(); //Liste des domaines associés
     }
 }
