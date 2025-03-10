ed3eaf92dcfdb6b798c91e0e329a18d1e5a0c2d9
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-10 11:49:28 +0100

    Added Content to Post  Entity

---
+++ first-aspnet/FirstAspnet.Domain/Entities/Post.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace firstAspnet.Domain.Entities
+{
+    public class Post {
+        [Column("Post_id")]
+        public int Id {get;set;}
+        [Column("POST_userId")]
+        public int UserId {get;set;}
+        [Column("POST_title")]
+        public  required string Title {get;set;}
+        [Column("POST_content")]
+        public  required string Content {get;set;}
+        [Column("POST_createddAt")]
+        public DateTime CreatedAt {get;set;}
+        [Column("POST_updated1t")]
+        public DateTime UpdateAt {get;set;}
+        
+    }
+}
