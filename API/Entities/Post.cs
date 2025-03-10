5ce40627f5a141f32e1784017ac65a4a536a66b1
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-10 15:08:51 +0100

    FEAT🚀 Added constraints to Post class

--- API/Entities/Post.cs
+++ API/Entities/Post.cs
@@ -1,19 +1,31 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Entities
 {
     public class Post 
     {
+        [Required]
         [Column("POST_id")]
         public int Id {get;set;}
+
+        [Required]
         [Column("POST_userId")]
+
         public int UserId {get;set;}
+
+        [Required]
         [Column("POST_title")]
         public  required string Title {get;set;}
+        
+        [Required]
         [Column("POST_content")]
-        public  required string Content {get;set;}
+        public required string Content {get;set;}
+        
+        [Required]
         [Column("POST_createddAt")]
         public DateTime CreatedAt {get;set;}
+        
         [Column("POST_updated1t")]
         public DateTime UpdateAt {get;set;}
         
