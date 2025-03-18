5f08840790da1a0a3933971ae71afb90b28e8b02
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-18 16:21:40 +0100

    STRUCT📁: Created 'Pagination' class in Services directory.

---
+++ API/Dtos/Responses/Pagination.cs
@@ -0,0 +1,14 @@
+
+
+using API.Enums;
+
+namespace API.Dtos.Responses
+{
+    public class Pagination<T>
+    {
+        public List<T> Data {get; set;} = new List<T>();
+        public int Page {get;set;}
+        public int Total {get;set;}
+        public EErrorType ErrorType {get;set;} = EErrorType.SUCCESS;
+    }
+}
