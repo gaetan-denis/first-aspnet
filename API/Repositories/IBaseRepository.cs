4fe56a41062d177da15b2082bf6b8cfa590d5cff
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-18 12:02:26 +0100

    FEAT🚀: Implemented BaseRepository interface to standardize repository operations.

---
+++ API/Repositories/IBaseRepository.cs
@@ -0,0 +1,15 @@
+
+
+using Azure.Identity;
+
+namespace API.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
