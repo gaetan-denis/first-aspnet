a844e460ad8ead97aa27bc45dd0a39735d43758d
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-19 11:46:30 +0100

    STRUCT 📁 Created 'Validador' folder, added 'PayloadValidator' file, and implemented 'ProtectAgainstSqlInjections' method

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbcbbe519046d632a84a7b7ace55c828a8e6f2cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef0a410243f8d4acb3201b0adb92386cda59b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
