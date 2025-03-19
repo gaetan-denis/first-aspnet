6d90f4c34c13e3b5dc569cc79fb7309f23d80db2
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-19 11:53:20 +0100

    STYLE 🎨 Fixed spelling mistakes in a comment

--- API/Validator/PayloadValidator.cs
+++ API/Validator/PayloadValidator.cs
@@ -1,12 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.Validator
 {
     public class PayloadValidator
     {
-        
+        /// <summary>
+        /// Prend une chaïne de caractèreet vérifie qu'elle ne contient pas de termes suscptibles de représenter une menace d'injection SQL.
+        /// </summary>
+        /// <param name="suspiciousString">La chaîne de caractère à analyser</param>
+        /// <returns>false si l'un des caractères dangereux et inclus dans `suspiciousString`, true si la chaîne est sécurisée</returns>
+        public static bool ProtectAgainSqlInjections(string suspiciousString)
+        {   
+            suspiciousString = suspiciousString.Trim();
+            string[] dangerousPatterns = new string[]
+            {
+                "SELECT", "INSERT", "DELETE", "UPDATE", "DROP", "TRUNCATE", "EXEC", "UNION",
+                "--", ";", "'", "\"", "/*", "*/", "xp_"
+            };
+            foreach (var pattern in dangerousPatterns)
+            {
+                if (suspiciousString.IndexOf(pattern, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
     }
 }
