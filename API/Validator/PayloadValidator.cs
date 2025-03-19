32e9e259fb9a780991ecc534f5b779ae0ffa53c1
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-19 13:37:23 +0100

    FEAT 🚀 Added 'BlockTemporaryEmails' method and improved class comments for better understanding

--- API/Validator/PayloadValidator.cs
+++ API/Validator/PayloadValidator.cs
@@ -6,7 +6,7 @@ namespace API.Validator
         /// Prend une chaïne de caractèreet vérifie qu'elle ne contient pas de termes suscptibles de représenter une menace d'injection SQL.
         /// </summary>
         /// <param name="suspiciousString">La chaîne de caractère à analyser</param>
-        /// <returns>false si l'un des caractères dangereux et inclus dans `suspiciousString`, true si la chaîne est sécurisée</returns>
+        /// <returns>false si l'un des caractères dangereux et inclus dans (<paramref name="suspiciousString"/>, true si la chaîne est sécurisée</returns>
         public static bool ProtectAgainstSQLI(string suspiciousString)
         {
             suspiciousString = suspiciousString.Trim();
@@ -31,7 +31,7 @@ namespace API.Validator
         /// Prend une chaîne de caractères et vérifie qu'elle ne contient pas de termes susceptibles de représenter une menace d'attaque XSS.
         /// </summary>
         /// <param name="suspiciousString">La chaîne de caractères à analyser</param>
-        /// <returns>false si l'un des caractères dangereux est inclus dans `suspiciousString`, true si la chaîne est sécurisée</returns>
+        /// <returns>False si l'un des caractères dangereux est inclus dans (<paramref name="suspiciousString"/>, true si la chaîne est sécurisée</returns>
         public static bool ProtectAgainstXSS(string suspiciousString)
         {
             if (string.IsNullOrWhiteSpace(suspiciousString))
@@ -59,6 +59,70 @@ namespace API.Validator
 
             return true;
         }
+        
+        /// <summary>
+        /// <Prend une chaîne de caractères (<paramref name="suspiciousEmail"/> et vérifie qu'elle ne contient pas de nom de domaine assimilé à une adresse mail jetable.>
+        /// </summary>
+        /// <param name="suspiciousEmail">La chaîne de caractères à analyser</param>
+        /// <returns>False si l'email provient d'un domaine de type mail jetable, true si l'email est valide.</returns>
 
+        public static bool BlockTemporaryEmails(string suspiciousEmail)
+        {
+            suspiciousEmail= suspiciousEmail.Trim();
+            string[] temporatyEmailDomain = new string []
+            {
+                "10minutemail.com",
+                "20minutemail.com",
+                "30minutemail.com",
+                "anonymail.net",
+                "bouncemail.com",
+                "burnermail.com",
+                "discard.email",
+                "disposableemail.com",
+                "dropmail.me",
+                "emailondeck.com",
+                "fakeinbox.com",
+                "getairmail.com",
+                "guerrillamail.com",
+                "hottempmail.com",
+                "incognitomail.com",
+                "instant-email.org",
+                "mail-temp.com",
+                "mail1a.de",
+                "mailcatch.com",
+                "maildrop.cc",
+                "mailinator.com",
+                "mailnesia.com",
+                "moakt.com",
+                "my10minutemail.com",
+                "mytrashmail.com",
+                "nowmymail.com",
+                "sharklasers.com",
+                "spam4.me",
+                "temp-mail.org",
+                "tempmail.com",
+                "tempmail.de",
+                "tempmail.net",
+                "throwawaymail.com",
+                "trash-mail.com",
+                "trashmail.com",
+                "trashmail.de",
+                "yopmail.com",
+                "yopmail.fr",
+                "yopmail.net",
+                "zmail.com"
+            };
+
+            foreach (var domain in temporatyEmailDomain)
+            {
+                if (suspiciousEmail.EndsWith(domain))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        }
     }
-}
