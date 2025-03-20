6383c32dbdf294fe2d2c9c25a59e0556da940eef
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-20 17:23:47 +0100

    FEAT🚀 Add deleteUser function in api.ts

--- Client/src/services/api.ts
+++ Client/src/services/api.ts
@@ -2,11 +2,21 @@ import { ApiResponse } from "../types/ApiResponse";
 import { Post } from "../types/Post";
 import { User } from "../types/User";
 
+// Users
+
 export const fetchAllUsers = async() : Promise<ApiResponse<User>> => {
     const response = await fetch("http://localhost:5086/api/v1/users")
     const data = await response.json();
     return data;
 }
+
+export const deleteUser = async(userID : string) : Promise<void> => {
+    const response = await fetch("httpo://localhost:5086/v1/users/{userId}",{
+        method : "DELETE",
+    });
+}
+
+
 
 export const fetchAllPosts = async() : Promise<ApiResponse<Post>> => {
     const response = await fetch('http://localhost:5086/api/v1/posts')
