396790704ca49baca8f4f59de7c1979c8a0742df
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-20 10:35:09 +0100

    CHORE🧹 Push modified assembly info files

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca72495ede3b55c40df7bd13bb3228d7ae9aa33c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12fa2db3c0659c05b469976a22efaf591b46ea99")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
