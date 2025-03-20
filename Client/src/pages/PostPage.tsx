39a61f7357fb7ec923e27fd4baa132c464ebeff5
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-20 15:40:41 +0100

    FEAT🚀 Create functional PostPage to display all posts

--- Client/src/pages/PostPage.tsx
+++ Client/src/pages/PostPage.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { ApiResponse } from "../types/ApiResponse";
+import { Post } from "../types/Post";
+import { fetchAllPosts } from "../services/api";
+
 const PostsPage: React.FC = () => {
-    return <h1>Posts List</h1>;
-  }
-  
-  export default PostsPage;
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetchAllPosts().then((response: ApiResponse<Post>) => {
+      console.log(response);
+      setPosts(response.data.data);
+    });
+  }, []);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Id Utilisateur</th>
+            <th>Titre</th>
+            <th>Contenu</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr>
+              <td>{post.userId}</td>
+              <td>{post.title}</td>
+              <td>{post.content}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default PostsPage;
